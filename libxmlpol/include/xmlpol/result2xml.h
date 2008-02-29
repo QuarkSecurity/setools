f81dcb14efee0beb38262eae26405504c2761860
Jason Tang <jtang@tresys.com>
2008-02-29 16:42:06 +0000

    * Synced libqpol to libsepol version 2.0.23, policy parser to         2.0.10.
    git-svn-id: http://oss.tresys.com/repos/setools/branches/setools-devel@4780 c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1

--- libxmlpol/include/xmlpol/result2xml.h
+++ libxmlpol/include/xmlpol/result2xml.h
@@ -8,7 +8,7 @@
 #include <apol/relabel-analysis.h>
 #include <apol/types-relation-analysis.h>
 
-extern const XMLDoc domain_trans_result_to_xml(domain_trans_result_t *result, XMLDoc doc);
-extern const XMLDoc infoflow_result_to_xml(inflow_result_t *result, XMLDoc doc);
-extern const XMLDoc relabel_result_to_xml(relabel_result_t *result, XMLDoc doc);
-extern const XMLDoc types_relation_result_to_xml(types_relation_result_t *result, XMLDoc doc);
+extern const XMLDoc domain_trans_result_to_xml(domain_trans_result_t * result, XMLDoc doc);
+extern const XMLDoc infoflow_result_to_xml(inflow_result_t * result, XMLDoc doc);
+extern const XMLDoc relabel_result_to_xml(relabel_result_t * result, XMLDoc doc);
+extern const XMLDoc types_relation_result_to_xml(types_relation_result_t * result, XMLDoc doc);
--- libxmlpol/src/result2xml.c
+++ libxmlpol/src/result2xml.c
@@ -4,18 +4,18 @@
 
 #include <xmlpol/resultsxml.h>
 
-extern const XmlDocument create_xmlpol() 
+extern const XmlDocument create_xmlpol()
 {
 	XmlDocument xmlDoc = new XmlDocument();
-	XmlDeclaration xmlDeclaration = xmlDoc.CreateXmlDeclaration("1.0","utf-8",null); 
-	XmlElement rootNode  = xmlDoc.CreateElement("Apol Analysis Report");
-	xmlDoc.InsertBefore(xmlDeclaration, xmlDoc.DocumentElement); 
+	XmlDeclaration xmlDeclaration = xmlDoc.CreateXmlDeclaration("1.0", "utf-8", null);
+	XmlElement rootNode = xmlDoc.CreateElement("Apol Analysis Report");
+	xmlDoc.InsertBefore(xmlDeclaration, xmlDoc.DocumentElement);
 	xmlDoc.AppendChild(rootNode);
 
 	return xmlDoc;
 }
 
-extern const XmlDocument domain_trans_result_to_xml(apol_domain_trans_result_t *result, XmlDocument xmlDoc) 
+extern const XmlDocument domain_trans_result_to_xml(apol_domain_trans_result_t * result, XmlDocument xmlDoc)
 {
 	if (!xmlDoc)
 		xmlDoc = create_xmlpol();
@@ -23,16 +23,16 @@ extern const XmlDocument domain_trans_result_to_xml(apol_domain_trans_result_t *
 	/* 
 	 * First add the start, entrypoint, and end types to the document.
 	 */
-        XmlElement parentNode  = xmlDoc.CreateElement("Domain Trans Result");
+	XmlElement parentNode = xmlDoc.CreateElement("Domain Trans Result");
 	parentNode.SetAttribute("Start Type",);
 	parentNode.SetAttribute("Entrypoint Type",);
 	parentNode.SetAttribute("End Type",);
-        xmlDoc.DocumentElement.PrependChild(parentNode);
+	xmlDoc.DocumentElement.PrependChild(parentNode);
 
 	return xmlDoc;
 }
 
-extern const XmlDocument infoflow_result_to_xml(infoflow_result_t *result, XmlDocument xmlDoc) 
+extern const XmlDocument infoflow_result_to_xml(infoflow_result_t * result, XmlDocument xmlDoc)
 {
 	if (!xmlDoc)
 		xmlDoc = create_xmlpol();
@@ -40,18 +40,18 @@ extern const XmlDocument infoflow_result_to_xml(infoflow_result_t *result, XmlDo
 	return xmlDoc;
 }
 
-extern const XmlDocument relabel_result_to_xml(relabel_result_t *result, XmlDocument xmlDoc) 
+extern const XmlDocument relabel_result_to_xml(relabel_result_t * result, XmlDocument xmlDoc)
 {
 	if (xmlDoc)
-		xmlDoc = create_xmlpol();	
+		xmlDoc = create_xmlpol();
 
 	return xmlDoc;
 }
 
-extern const XmlDocument types_relation_result_to_xml(types_relation_result_t *result, XmlDocument xmlDoc) 
+extern const XmlDocument types_relation_result_to_xml(types_relation_result_t * result, XmlDocument xmlDoc)
 {
 	if (!XmlDocument)
-		xmlDoc = create_xmlpol(); 
+		xmlDoc = create_xmlpol();
 
 	return XmlDocument;
 }
